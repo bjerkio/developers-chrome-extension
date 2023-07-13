4badcc23f8a5be466e1f9265caa6014a1834c6b9
Simen A. W. Olsen <so@bjerk.io>
2023-07-14 01:34:01 +0200

    wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,25 @@
 module.exports = {
-  extends: ['@bjerk/eslint-config', 'plugin:jest/recommended'],
-  plugins: ['jest'],
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['eslint:recommended', '@bjerk/eslint-config'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
   overrides: [
     {
-      files: 'jest.config.*',
+      files: ['vite.config.ts', 'manifest.config.ts'],
       rules: {
         'import/no-default-export': 'off',
       },
     },
   ],
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
 };
